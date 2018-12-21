bd777f836b325188c8465f09511babc03168bc2a
Sergey Kryukov <cydkom@gmail.com>
2018-12-22 00:35:17 +0300

    math.ceil

--- check_please.py
+++ check_please.py
@@ -1,5 +1,8 @@
+import math
+
+
 def splite_check(total, number_of_people):
-    cost_per_person = total / number_of_people
+    cost_per_person = math.ceil (total / number_of_people)
     return cost_per_person
 
 amount_due = splite_check(84.97, 4)
